1536f81dbae820c6d5ed0bd9126ac9e025f71e2f
FrogMaster <kotypierdoloty@gmail.com>
2026-03-10 19:23:12 +0100

    wypychanie

---
+++ RandomNumbers/Converters/Converters.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Globalization;
+
+namespace RandomNumbers.Converters
+{
+    // tekst szary jeśli nieobecny
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isPresent = (bool)value;
+            return isPresent ? Colors.Black : Colors.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+    }
+
+    // tło zielone jeśli szczęśliwy numer
+    public class LuckyNumberColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool hasLucky = (bool)value;
+            return hasLucky ? Colors.LightGreen : Colors.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+    }
+}
